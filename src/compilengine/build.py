77ec785230296f4a151e52eae81aded4a2881466
Barrett <barrettbecker19@gmail.com>
2026-04-15 13:38:34 -0400

    fixed compilengine import

--- src/compilengine/build.py
+++ src/compilengine/build.py
@@ -1,11 +1,18 @@
 import tomllib
-import cli
+from .. import cli
+
 
 def build(name: str, author: str):
-    REGISTRY_BASE = "https://raw.githubusercontent.com/sawn1c-repos/registry/main/packages"
+    REGISTRY_BASE = (
+        "https://raw.githubusercontent.com/sawn1c-repos/registry/main/packages"
+    )
     url = f"{REGISTRY_BASE}/{author}/{name}.toml"
     tomllib.load(url)
     if author not in url or author == "":
-        cli.error(f"Package {name} by {author} requires a valid author (to specify the package)")
+        cli.error(
+            f"Package {name} by {author} requires a valid author (to specify the package)"
+        )
     elif name not in url or name == "":
-        cli.error(f"Package {name} by {author} requires a valid name (to specify the package)")
+        cli.error(
+            f"Package {name} by {author} requires a valid name (to specify the package)"
+        )
