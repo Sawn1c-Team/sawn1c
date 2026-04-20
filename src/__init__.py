3135860ece7b6b64a6c08459cf207c8decf7f269
Barrett <barrettbecker19@gmail.com>
2026-04-19 20:21:26 -0400

    fixed a __init__.py issue in src, the # in the code is mainly to show the issues ruff showed me

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
-import cli as cli
-import platform_check as platform_check
-import reinstall as reinstall
-import security as security
+from . import cli
+from . import platform_check
+from . import reinstall
+from . import security
+
+__all__ = ["cli", "platform_check", "reinstall", "security"]
